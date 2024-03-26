88a476d32140c1bf79f3e4f25e7022a150de73bd
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-03-26 23:46:16 +0100

    add content

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="start-page">
+    <header class="flex justify-between">
+      <img src="@/assets/images/vtg-logo.png" alt="vtg musli logo" />
+      <div>
+        <a href="">Angebot</a>
+        <a href="">Finanzierung & Garantie</a>
+        <a href="">Über Uns</a>
+        <a href="">Kontakt</a>
+        <a href="">Bewertung</a>
+      </div>
+    </header>
+    <h1>Automobile</h1>
+    <h2>Entspannt unterwegs:<br />Qualität für sichere Fahrten!</h2>
+    <div>
+      <p>Unsere Fahrzeuge:</p>
+      <p><a href="">mobile<span>.</span>de</a></p>
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="angebot-page" class="grid grid-cols-2 gap-y-4 gap-x-8">
+    <div>
+      <div>icon</div>
+      <p>Das passende Fahrzeug für Sie</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Professionelle Fahrzeugaufbereitung</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Ausgebiege Probefahrt</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Inzahlungnahme des alten Fahrzeugs</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Finanzdienstleistungen</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Zulassungsservice, An- und Abmeldung</p>
+    </div>
+
+    <div>
+      <div>icon</div>
+      <p>Bis zu 24 Monate Gebrauchtwagengarantie</p>
+    </div>
+  </div>
+
+  <div
+    id="finanzierung-garantie"
+    class="grid grid-cols-2 gap-x-10 gap-y-10 items-center"
+  >
+    <img
+      src="@/assets/images/realgarant.png"
+      alt="logo Real Garant"
+      class="max-h-20 mx-auto"
+    />
+    <img
+      src="@/assets/images/huk_coburg.png"
+      alt="logo Huk Coburg"
+      class="max-h-20 mx-auto"
+    />
+    <img
+      src="@/assets/images/santander_carcredit.png"
+      alt="logo Santander"
+      class="max-h-20 mx-auto"
+    />
+    <img
+      src="@/assets/images/multipart.png"
+      alt="logo multipart"
+      class="max-h-20 mx-auto"
+    />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
