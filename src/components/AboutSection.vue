d4b334017f94cc0bd7d9c216d7563e00d9f14ce8
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-08-07 14:16:07 +0200

    refactor: delete links of FinanzierungSection

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -2,7 +2,7 @@
   <div id="about-section" class="bg-light-blue px-8 lg:px-16">
     <h1 class="pt-16">Über Uns</h1>
     <p class="pt-24 text-[2rem]">Wilkommen auf unserer Homepage!</p>
-    <p class="pt-20 text-[2rem] max-w-[1300px]">
+    <p class="pt-20 text-[2rem]">
       Wir unterstützen Sie gerne bei der Suche nach Ihrem neuen Fahrzeug. Mit
       über 20 Jahren Erfahrung in der Automobilbranche sind wir bestens
       gerüstet, um Ihre Anforderungen zu erfüllen.<br />
--- src/components/FinanzierungSection.vue
+++ src/components/FinanzierungSection.vue
@@ -4,30 +4,28 @@
     <div
       class="my-40 grid grid-cols-1 xl:grid-cols-2 gap-x-10 gap-y-40 items-center relative"
     >
-      <a href="https://autohelden.com/" target="_blank">
-        <img
-          src="@/assets/images/autohelden-logo.png"
-          alt="logo Autohelden"
-          class="max-h-40 hover:scale-110 duration-300 mx-auto"
-      /></a>
-      <a href="https://www.huk.de/" target="_blank"
-        ><img
-          src="@/assets/images/huk_coburg.png"
-          alt="logo Huk Coburg"
-          class="max-h-40 mx-auto hover:scale-110 duration-300"
-      /></a>
-      <a href="https://www.santander.de/privatkunden/" target="_blank">
-        <img
-          src="@/assets/images/santander_carcredit.png"
-          alt="logo Santander"
-          class="max-h-40 hover:scale-110 duration-300"
-      /></a>
-      <a href="https://mobile-garantie.de/" target="_blank"
-        ><img
-          src="@/assets/images/logo-mobile-garantie.svg"
-          alt="logo mobile garantie"
-          class="max-h-32 mx-auto hover:scale-110 duration-300"
-      /></a>
+      <img
+        src="@/assets/images/autohelden-logo.png"
+        alt="logo Autohelden"
+        class="max-h-40 mx-auto"
+      />
+      <img
+        src="@/assets/images/huk_coburg.png"
+        alt="logo Huk Coburg"
+        class="max-h-40 mx-auto"
+      />
+
+      <img
+        src="@/assets/images/santander_carcredit.png"
+        alt="logo Santander"
+        class="max-h-40 mx-auto"
+      />
+
+      <img
+        src="@/assets/images/logo-mobile-garantie.svg"
+        alt="logo mobile garantie"
+        class="max-h-32 mx-auto"
+      />
     </div>
   </div>
 </template>
